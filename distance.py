13d3f82ae33678fb2b51d56fbde6e6c4326fe30a
Angel-Zaldivar <ZaldivarAngel.2004@gmail.com>
2024-06-25 19:30:20 +0000

    pseudo code setup for light pollution

--- distance.py
+++ distance.py
@@ -1,1 +1,25 @@
 import requests
+import json
+
+
+
+counter = 5
+
+longitude = -108.230242
+latitude = 45.724115
+location = longitude,latitude
+radius = 20
+response = requests.get(f"http://getnearbycities.geobytes.com/GetNearbyCities?radius={radius}&latitude={latitude}&longitude={longitude}")
+dict = json.loads(response.text) 
+print(dict)
+
+if len(dict) > 5:
+    counter -= 2
+
+for key in dict:
+    response = requests.get("city")
+    dict2 = json.loads(response.text)
+    if dict2["population"] < 50,000:
+        continue
+    else:
+        counter -= 1
