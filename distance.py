25f8599b1e811708d34538ca0d90025158c5db54
Angel-Zaldivar <ZaldivarAngel.2004@gmail.com>
2024-06-25 20:21:26 +0000

    implement city nearby search and pop check

--- distance.py
+++ distance.py
@@ -2,24 +2,24 @@ import requests
 import json
 
 
-
 counter = 5
-
 longitude = -108.230242
 latitude = 45.724115
 location = longitude,latitude
 radius = 20
 response = requests.get(f"http://getnearbycities.geobytes.com/GetNearbyCities?radius={radius}&latitude={latitude}&longitude={longitude}")
 dict = json.loads(response.text) 
-print(dict)
 
 if len(dict) > 5:
     counter -= 2
-
-for key in dict:
-    response = requests.get("city")
-    dict2 = json.loads(response.text)
-    if dict2["population"] < 50,000:
-        continue
-    else:
-        counter -= 1
+else:
+    for element in dict:
+        name = element[1]
+        longitude = element[10]  
+        latitude = element[8]
+        api_url = f'https://api.api-ninjas.com/v1/city?name={name}'
+        response1 = requests.get(api_url,headers={'X-Api-Key':"ex8H0bQ796On3t6f4lrWbw==DqHZjWAAgliP1fGH"})
+        dict2 = json.loads(response1.text)
+        pop = dict2[0]["population"]
+        if pop >= 50,000:
+            counter -= 2
