e081af2663a39423e1609752e0a6f7217f2d880d
Angel-Zaldivar <ZaldivarAngel.2004@gmail.com>
2024-06-26 18:55:52 +0000

    Add db class, improve prompt system

--- distance.py
+++ distance.py
@@ -2,40 +2,78 @@ import requests
 import json
 import sqlite3
 import os
+import sys
 
 #add location
-def add_location(connection,location):
-    sql = '''INSERT INTO saved_locations(name,longitude,latitude)
-            VALUES(?,?,?)'''
-    cur = connection.cursor()
-    cur.execute(sql,location)
-    connection.commit()
-    return cur.lastrowid
+class DB():
+    def __init__(self,db_name):
+        self.connection = sqlite3.connect(db_name)
+        self.cur = self.connection.cursor()
+        
+    def saved_locations(self):
+        self.cur.execute("SELECT * FROM saved_locations")
+        rows = self.cur.fetchall()
+        if rows == None:
+            return 0
+        else:
+            return rows
+    
+    def print_rows(self):
+        rows = self.saved_locations()
+        if rows == 0:
+            print("No locations have been saved")
+        else:
+            for row in rows:
+                print(row)
+
+    def add_location(self,location):
+        sql = '''INSERT INTO saved_locations(name,longitude,latitude)
+                VALUES(?,?,?)'''
+        self.cur.execute(sql,location)
+        self.connection.commit()
+        return cur.lastrowid
+
+class Prompt():
+    def __init__(self,latitude,longitude):
+        self.location
 
 counter = 5
 worst = False
-location_longitude = -108.230242
-location_latitude = 45.724115
+city_counter = 0
+location_longitude = None
+location_latitude = None
+a = 45.724115
+b = -108.230242
 radius = 20
 #response = requests.get(f"http://getnearbycities.geobytes.com/GetNearbyCities?radius={radius}&latitude={latitudes}&longitude={longitudes}")
 #dict = json.loads(response.text) 
-
 #db connection
-connection = sqlite3.connect("star.db")
-cur = connection.cursor()
-cur.execute("SELECT * FROM saved_locations")
-rows = cur.fetchall()
+#connection = sqlite3.connect("star.db")
+#cur = connection.cursor()
+#cur.execute("SELECT * FROM saved_locations")
+#rows = cur.fetchall()
 #intro prompt
+var = DB("star.db")
+var.print_rows()
 print("Hello!, Welcome to the star gazer app!")
 print("Your saved locations: ")
-for row in rows:
-    print(row)
 #Asking for longitude and lattitude
 response = int(input("Enter 0 to lookup a new location or enter a number matching a location to look up a saved location: "))    
+print("if you would like to quit enter \q at anytime a prompt is given")
 if response == 0:
-    location_latitude = float(input("Enter a latitude: "))
-    location_longitude = float(input("Enter a longitude: "))
-    
+    while not isinstance(location_latitude,float) or not isinstance(location_longitude,float):
+        location_latitude = input("Enter a latitude: ")
+        if location_latitude == "\q":
+            print("cya l8ter alligator")
+            sys.exit()
+        location_longitude = input("Enter a longitude: ")
+        if location_longitude == "\q":
+            print("cya l8ter alligator")
+            sys.exit()
+        if not isinstance(location_latitude,float):
+            print("please enter a valid latitude decimal value")
+        if not isinstance(location_longitude,float):
+            print("please enter a valid longitude decimal value")
 else:
     entry = response - 1
     if 0 > entry >= len(rows):
@@ -48,6 +86,7 @@ print(location_longitude)
 
 #if len(dict) > 5:
     #counter -= 2
+    #city_counter += 6
 #else:
     #for element in dict:
         #name = element[1]
@@ -64,12 +103,13 @@ print(location_longitude)
             #break
 
 #score calculation/optimal for star gazing or not?            
-score_card = ("Location not optimal for stargazing")
-
+#def scoreCalculation(city_counter,Point
+        
 resp = input("Would you like to save the location(y)(n)?")
 if resp == "y":
     name_given = input("What would you like to name this location?")
     location = (name_given,location_longitude,location_latitudes)
     add_location(connection,location)
-else:
-    print("Thanks for using the application")
+elif resp == "n":
+    resp1 = input("would you like to try looking up another location(y)(n)?")
+
