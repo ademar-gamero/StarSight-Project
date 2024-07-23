06a245795344ab10d475bb508cf2abf094c7fbb7
sofieutoft <sofieu26@gmail.com>
2024-07-23 14:49:45 -0500

    Fixed formatting

--- constellation.py
+++ constellation.py
@@ -184,7 +184,7 @@ def populate_constellations_table(db, Constellation):
                 "img":"Andromeda.jpg"
                 },
                 {
-                "name":"UrsaMinor",
+                "name":"Ursa Minor",
                 "description":"Ursa Minor (Latin: 'Lesser Bear', contrasting with Ursa Major), also known as the Little Bear, is a constellation located in the far northern sky. As with the Great Bear, the tail of the Little Bear may also be seen as the handle of a ladle, hence the North American name, Little Dipper: seven stars with four in its bowl like its partner the Big Dipper. Ursa Minor was one of the 48 constellations listed by the 2nd-century astronomer Ptolemy, and remains one of the 88 modern constellations. Ursa Minor has traditionally been important for navigation, particularly by mariners, because of Polaris being the north pole star.",
                 "img":"UrsaMinor.jpg"
                 },
