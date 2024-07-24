079410c8d5968f0c87433b01ae881ad9125edd74
sofieutoft <sofieu26@gmail.com>
2024-07-24 01:30:20 -0500

    Fixed UI

--- forms.py
+++ forms.py
@@ -7,7 +7,7 @@ cur_usr = curr_user()
 class LocationForm(FlaskForm):
     loc_lat = DecimalField("lat", places=12,rounding=None)
     loc_lon = DecimalField("lng", places=12,rounding=None)
-    loc_radius = SelectField('Choose a search radius', choices=[(8.04672,'5 miles'),(16.0934,'10 miles'),(32.1869,'20 miles')], coerce=float)
+    loc_radius = SelectField('Choose a search radius: ', choices=[(8.04672,'5 miles'),(16.0934,'10 miles'),(32.1869,'20 miles')], coerce=float)
     submit = SubmitField('Find Stars')
     
 class RegistrationForm(FlaskForm):
